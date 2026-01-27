e55acf32601ae58c1caafa13e50e093e062bc21e
luccaGulua <gulualuka0@gmail.com>
2026-01-27 08:37:16 +0400

    fromhome 27.1.26

---
+++ app/src/Components/Loads/PlaceHolder/GridPlaceHolder.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+export const GridPlaceHolder = ({ lenght }) => {
+  return (
+    <div className="flex justify-center mt-28 ">
+      <div className=" grid grid-cols-1 ssm:grid-cols-2 mmd:grid-cols-3 gap-y-20 gap-4   ssm:gap-10 xl:grid-cols-4 xl:gap-10 xxl:gap-20 ">
+        {Array.from({ length: lenght }).map((_, index) => (
+          <div
+            key={index}
+            className=" max-w-sm p-4 border border-gray-200 rounded w-[380px]  h-[400px] shadow animate-pulse md:p-6 dark:border-gray-400"
+          >
+            <div className="flex items-center justify-center h-3/4 mb-4 bg-gray-300 rounded dark:bg-gray-400">
+              <svg
+                viewBox="0 0 16 20"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+                className="w-10 h-10 text-gray-200 dark:text-gray-600"
+              >
+                <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z" />
+                <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z" />
+              </svg>
+            </div>
+
+            <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-400 w-48 mb-4"></div>
+            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-400 mb-2.5"></div>
+            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-400 mb-2.5"></div>
+            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-400"></div>
+            <span className="sr-only">Loading...</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
--- app/src/Pages/Products/Products.jsx
+++ app/src/Pages/Products/Products.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useFetchData } from "../../Hooks/useFetchData";
 import { ErrorLoader } from "../../Components/Loads/error/ErrorLoader";
 import { MainGridProduct } from "../../Components/Products/MainGridProduct";
+import { GridPlaceHolder } from "../../Components/Loads/PlaceHolder/GridPlaceHolder";
 
 export const Products = () => {
   const [data, error, isLoading] = useFetchData(
@@ -12,12 +13,41 @@ export const Products = () => {
   }
 
   return (
-    <div className="flex justify-center mt-28 ">
-      <div className=" grid grid-cols-1 ssm:grid-cols-2 mmd:grid-cols-3 gap-y-20 gap-4   ssm:gap-10 xl:grid-cols-4 xl:gap-10 xxl:gap-20 ">
-        {data.map((item) => (
-          <MainGridProduct props={item} />
-        ))}
-      </div>
-    </div>
+    <>
+      {isLoading ? (
+        <div>
+          <GridPlaceHolder lenght={data.length} />
+          <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/60">
+            <div className="flex items-center justify-center gap-6">
+              <div className="load">
+                <svg viewBox="0 0 80 80">
+                  <circle cx="40" cy="40" r="32" />
+                </svg>
+              </div>
+
+              <div className="load triangle">
+                <svg viewBox="0 0 86 80">
+                  <polygon points="43 8 79 72 7 72" />
+                </svg>
+              </div>
+
+              <div className="load">
+                <svg viewBox="0 0 80 80">
+                  <rect x="8" y="8" width="64" height="64" />
+                </svg>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className="flex justify-center mt-28 ">
+          <div className=" grid grid-cols-1 ssm:grid-cols-2 mmd:grid-cols-3 gap-y-20 gap-4   ssm:gap-10 xl:grid-cols-4 xl:gap-10 xxl:gap-20 ">
+            {data.map((item) => (
+              <MainGridProduct props={item} />
+            ))}
+          </div>
+        </div>
+      )}
+    </>
   );
 };
