99b6400031109611a921a839f3a82f266628ff75
luccaGulua <gulualuka0@gmail.com>
2026-04-14 00:12:02 +0400

    middleware

--- server/Routes/auth.js
+++ server/Routes/auth.js
@@ -2,7 +2,7 @@ import express from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { UserModel } from "../Models/users.js";
-import authMiddleware from "./authmiddleware.js";
+import authMiddleware from "./authMiddleware.js";
 const router = express.Router();
 
 // import userDb from "../Config/db";
